d221bd01855cd0938866c6d094b74f8cfc4b7732
saloni1108 <saloni1108goyal@gmail.com>
2024-04-20 11:59:04 +0530

    [Saloni] Add: Python script to check whether the year given is a leap year or not

--- 3_Leap_Year.py
+++ 3_Leap_Year.py
@@ -3,20 +3,19 @@ This file contains  pythno script to identify whether the given year is a leap y
 """
 
 def leap_year(year):
-    if len(str(year)) == 4:
-        try:
-            if (year % 400 == 0) or (year % 100 != 0) and (year % 4 == 0):
-                print("The given year ", year, " is a leap year")
-            else: 
-                print("The yeat ", year, " is not a leap year")
-        except Exception as e:
-            return e
-    else:
-        print("Enter the valid year")
-        return main()
+        if (year % 400 == 0) or (year % 100 != 0) and (year % 4 == 0):
+            print("The given year ", year, " is a leap year")
+        else:
+            print("Year is not a leap year")
         
 def main():
     year_to_check = int(input("Enter the year: "))
+    try:
+        if len(str(year_to_check)) != 4:
+            raise ValueError("Enter a valid year")
+    except Exception as e:
+         print("The Exception occured is: ", e) 
+         return main()
     leap_year(year_to_check)
 
 if __name__ == "__main__":
